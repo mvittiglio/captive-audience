c6b6e4c76f41ee0ba3b72bfa5e5482a6132c42e5
PilZero <43547279+PilZero@users.noreply.github.com>
2025-01-15 12:18:14 -0500

    added audio

--- Assets/IntroScreens/Scenes/SimpleMoverBehavior.cs
+++ Assets/IntroScreens/Scenes/SimpleMoverBehavior.cs
@@ -5,12 +5,14 @@ public class SimpleMoverBehavior : MonoBehaviour
     public float speed = 1.0f;
     public Transform target = null;
     private float foo = 0;
+    private float bar = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //MARK PIL WUZ HERE
         foo = 69f; //nice
+        bar = 420;
     }
 
     // Update is called once per frame
